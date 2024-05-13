159ab181b0cbc25d7f38e0f7541494f859dc032f
s3818058 <s3818058@rmit.edu.vn>
2024-05-13 13:37:01 +0700

    login.fxml and main.fxml implemented

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,47 @@
  * @author <Martin Delahousse - s4034308>
  */
 
-import helper.Printer;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import controller.loginController;
 
-import java.util.Scanner;
-
-public class Main {
+public class Main extends Application {
 
     static Manager manager;
 
-    public static void main(String[] args) {
-        //Create Scanner & Manager
-        Scanner in = new Scanner(System.in);
-        manager = new Manager();
-        String command;
-
-        //Intercept program stop to properly save data
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> manager.shutdown()));
-
-        //Quick function to display welcome information
-        welcome();
-
-        //Program loop
-        do {
-            command = in.nextLine();
-        } while (manager.exec(command));
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLs/Login.fxml"));
+        Parent root = loader.load();
+        loginController LoginController = loader.getController();
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
 
-    public static void welcome() {
-        Printer.hint("Welcome to our insurance claim helper");
-        Printer.hint("------------------------------------------");
-        manager.exec("help");
-    }
+//    public static void main(String[] args) {
+//        //Create Scanner & Manager
+//        Scanner in = new Scanner(System.in);
+//        manager = new Manager();
+//        String command;
+//
+//        //Intercept program stop to properly save data
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> manager.shutdown()));
+//
+//        //Quick function to display welcome information
+//        welcome();
+//
+//        //Program loop
+//        do {
+//            command = in.nextLine();
+//        } while (manager.exec(command));
+//    }
+//
+//    public static void welcome() {
+//        Printer.hint("Welcome to our insurance claim helper");
+//        Printer.hint("------------------------------------------");
+//        manager.exec("help");
+//    }
+
 }
---
+++ src/controller/loginController.java
@@ -0,0 +1,58 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class loginController {
+    @FXML
+    private TextField userID;
+    @FXML
+    private PasswordField userPassword;
+    @FXML
+    void loginBtnClicked(ActionEvent event) {
+        if (userID != null && userPassword != null) {
+            String ID = userID.getText();
+            String password = userPassword.getText();
+
+            if(ID.equals("admin") && password.equals("1234")) {
+                loadScene(event);
+            } else {
+                showAlert("Login Failed","Wrong ID or password entered");
+            }
+        } else {
+            System.out.println("textField 또는 passwordField 객체가 null입니다.");
+        }
+    }
+
+    private void loadScene(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../FXMLs/Main.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
